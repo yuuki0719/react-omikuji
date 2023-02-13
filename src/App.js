28e387c94724482208671fb35f7413662dcf3e73
yuuki0719 <yuuki20030719@gmail.com>
2023-02-13 23:02:31 +0900

    おみくじ作成

--- src/App.js
+++ src/App.js
@@ -13,4 +13,39 @@ const query = `
   }
 }
 `
+function App() {
+  const [page, setPage] = useState(null);
 
+  useEffect(() => {
+    window
+      .fetch(`https://graphql.contentful.com/content/v1/spaces/reb9eu4dhxhb/`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer S7DU5L-bAzhRQZ4bfI87_-sSYd6PC3M4Qiwn2Vz8mv4",
+        },
+        body: JSON.stringify({ query }),
+      })
+      .then((response) => response.json())
+      .then(({ data, errors }) => {
+        if (errors) {
+          console.error(errors);
+        }
+        const number = Math.floor( Math.random() * ((data.omikujiCollection.items.length - 1) + 1 - 0) ) + 0
+        setPage(data.omikujiCollection.items[number]);
+      });
+  }, []);
+
+  
+
+  return (
+    <div className="App">
+      <Router basename='/react-omikuji/'>
+        <Route exact path='/' render={() => <Button text='今日の運勢を占う'/>} />
+        <Route exact path='/result' render={() => <Result title={page.title} description={page.description}/>} />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
